59b3cb2f1a5f1adb928bb535e8237f8e6195fc11
Austin Diaz <austinddiaz@gmail.com>
2015-06-29 22:47:58 -0700

    Master

---
+++ BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Austin Diaz on 3/22/15.
+//  Copyright (c) 2015 Austin Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
